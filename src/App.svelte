f8d9bdb074c42d5c8c9aa920a793e9218ca7953e
beata <beata@emc2wave.com>
2020-09-07 15:47:04 -0700

    delete list <Title/> from App

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,8 @@
 <script>
   import Navbar from './Navbar.svelte';
-  import Title from './Title.svelte';
-  let fruits = ['apple', 'orange', 'lemon'];
 </script>
 
 <style>
 </style>
 
 <Navbar />
-
-{#each fruits as item}
-  <Title title={item} />
-{/each}
