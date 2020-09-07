632b498f5072673edf1beffebddc9f7449e37fa0
beata <beata@emc2wave.com>
2020-09-06 20:06:25 -0700

    cut navbar markup from App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -3,11 +3,3 @@
 
 <style>
 </style>
-
-<!-- Html -->
-<nav class="nav">
-  <div class="nav-center">
-    <h1 class="nav-title">budget calculator</h1>
-    <button type="button" class="nav-btn"><i class="far fa-plus-square" /> add item</button>
-  </div>
-</nav>
