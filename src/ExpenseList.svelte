917007bb5ecd59770da1e1ae594345ec6a4b956f
beata <beata@emc2wave.com>
2020-09-07 16:30:24 -0700

    add <ul>     {#each expenses as expense}{/each}   </ul>

--- src/ExpenseList.svelte
+++ src/ExpenseList.svelte
@@ -1,8 +1,12 @@
 <script>
   import SectionTitle from './Title.svelte';
+  import SingleExpense from './SingleExpense.svelte';
   export let expenses = [];
 </script>
 
 <section>
   <SectionTitle title="expense list" />
+  <ul>
+    {#each expenses as expense}{/each}
+  </ul>
 </section>
