87e9774d0e534bfddc815ea39b69ae9503fad783
beata <beata@emc2wave.com>
2020-09-07 16:08:51 -0700

    import ExpenseList to App

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,8 @@
 <script>
   // Components
   import Navbar from './Navbar.svelte';
+  import ExpenseList from './ExpenseList.svelte';
+  // data
   import expensesData from './expenses';
   // Veriables
   let expenses = [...expensesData];
