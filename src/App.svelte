97bb03acd414f2864c1c12c6a3fd25ebd95dbe1c
beata <beata@emc2wave.com>
2020-09-07 15:44:45 -0700

    rent list by usint <Title/>

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,6 @@
 <script>
   import Navbar from './Navbar.svelte';
+  import Title from './Title.svelte';
   let fruits = ['apple', 'orange', 'lemon'];
 </script>
 
@@ -9,5 +10,5 @@
 <Navbar />
 
 {#each fruits as item}
-  <h1>fruit: {item}</h1>
+  <Title title={item} />
 {/each}
