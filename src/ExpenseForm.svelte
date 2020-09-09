a5325b1be78aa4ec2873491bc4b9d103c67819c3
beata <beata@emc2wave.com>
2020-09-09 14:39:41 -0700

    add form

--- src/ExpenseForm.svelte
+++ src/ExpenseForm.svelte
@@ -3,6 +3,12 @@
   import Title from './Title.svelte';
 </script>
 
-<section>
+<section class="form">
   <Title title="add expense" />
+  <form class="expense-form">
+    <div class="form-control">
+      <label for="name">name</label>
+      <input type="text" id="name" />
+    </div>
+  </form>
 </section>
