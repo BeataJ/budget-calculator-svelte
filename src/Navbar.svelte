2fe05c190be0dec7f529a2e886b308002e35be78
beata <beata@emc2wave.com>
2020-09-12 22:14:00 -0700

    add export showForm to navbae.svelte

--- src/Navbar.svelte
+++ src/Navbar.svelte
@@ -1,6 +1,12 @@
+<script>
+  export let showForm;
+</script>
+
 <nav class="nav">
   <div class="nav-center">
     <h1 class="nav-title">budget calculator</h1>
-    <button type="button" class="nav-btn"><i class="far fa-plus-square" /> add item</button>
+    <button type="button" class="nav-btn"><i
+        class="far fa-plus-square"
+        on:click={showForm} /> add item</button>
   </div>
 </nav>
