d9df7b0415cc88818dc98070af260edc32787310
beata <beata@emc2wave.com>
2020-09-15 16:16:13 -0700

    use different transitions to SingleExpense h4

--- src/SingleExpense.svelte
+++ src/SingleExpense.svelte
@@ -22,7 +22,7 @@
       </button>
     </h2>
     {#if displayAmount}
-      <h4>amount: ${amount}</h4>
+      <h4 transition:fly>amount: ${amount}</h4>
     {/if}
   </div>
   <div class="expense-buttons">
