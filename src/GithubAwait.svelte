1cf79b7d7b4f703b710c266b6f24c6904ca67cb1
beata <beata@emc2wave.com>
2020-09-16 16:06:12 -0700

    fetch data by using svelte #await

--- src/GithubAwait.svelte
+++ src/GithubAwait.svelte
@@ -6,4 +6,20 @@
   };
 </script>
 
-<h2>Hello from githubawait</h2>
+<section>
+  {#await getUsers()}
+    <h2>...loading</h2>
+  {:then users}
+    {#each users as user}
+      <article class="user">
+        <img src={user.avatar_url} alt={user.login} />
+        <div class="user-info">
+          <h3>User: {user.login}</h3>
+          <a href={user.html_url} class="btn-primary" target="_blank">github url</a>
+        </div>
+      </article>
+    {/each}
+  {:catch error}
+    <p>Something went wrong: {error.message}</p>
+  {/await}
+</section>
