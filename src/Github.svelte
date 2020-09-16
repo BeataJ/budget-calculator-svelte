6f21cbd1a5acf1f71ed41b20753fc753c858e232
beata <beata@emc2wave.com>
2020-09-16 15:35:27 -0700

    add section article ..

--- src/Github.svelte
+++ src/Github.svelte
@@ -19,5 +19,15 @@
 {#if loading}
   <h2>Loading...</h2>
 {:else}
-  <h2>Data</h2>
+  <section>
+    {#each users as user}
+      <article class="user">
+        <img src={user.avatar_url} alt={user.login} />
+        <div class="user-info">
+          <h3>User: {user.login}</h3>
+          <a href={user.html_url} class="btn-primary" target="_blank">github url</a>
+        </div>
+      </article>
+    {/each}
+  </section>
 {/if}
