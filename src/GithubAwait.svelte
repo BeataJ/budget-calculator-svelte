bc9e356f2d83ad05a6389833267e846b1ac80cc7
beata <beata@emc2wave.com>
2020-09-16 15:48:12 -0700

    create getUsers()

--- src/GithubAwait.svelte
+++ src/GithubAwait.svelte
@@ -1,4 +1,9 @@
 <script>
+  const getUsers = async () => {
+    let userData = await fetch('https://api.github.com/users');
+    let githubUsers = await userData.json();
+    return githubUsers;
+  };
 </script>
 
 <h2>Hello from githubawait</h2>
